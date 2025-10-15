683d9f9a11e5f719362a63e438c9b62e9b765151
hallucinogenizer <rohanhussain1@yahoo.com>
2025-10-15 16:51:57 +0500

    fixed wrong rule used in index.mjs

--- index.mjs
+++ index.mjs
@@ -1,14 +1,14 @@
 import noHardcodedQueryParams from "./lib/rules/no-hardcoded-query-params.js";
 
 export const rules = {
-  "no-pl-pr-tailwind": noPlPrTailwindRule,
+  "no-hardcoded-query-params": noHardcodedQueryParams,
 };
 
 // Add this line for default export:
 export default {
   meta: {
     name: "eslint-plugin-no-hardcoded-query-params",
-    version: "1.0.0",
+    version: "2.0.0",
   },
   rules,
 };
